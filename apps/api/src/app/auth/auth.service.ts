0722903864eebb5042a33a5e438cb545fb75bff6
ThianHooi <63966221+ThianHooi@users.noreply.github.com>
2022-01-01 19:20:24 +0800

    feat: added auth service

---
+++ apps/api/src/app/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
+import { LoginUserInput } from './login-input.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findOneUserByEmail(email);
+
+    if (user && (await bcrypt.compare(user.password, password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async login(loginUserInput: LoginUserInput): Promise<any> {
+    const user = await this.userService.findOneUserByEmail(
+      loginUserInput.email,
+    );
+
+    const isPasswordMatch = await bcrypt.compare(
+      loginUserInput.password,
+      user.password,
+    );
+
+    if (user && isPasswordMatch) {
+      const { password, ...result } = user;
+      return {
+        access_token: this.jwtService.sign({ name: user.name, sub: user.id }),
+        user: result,
+      };
+    }
+
+    throw new UnauthorizedException();
+  }
+}
