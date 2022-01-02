26021c9984cc041f5e57d278e0eec9ac5c4244dd
ThianHooi <63966221+ThianHooi@users.noreply.github.com>
2022-01-02 21:09:57 +0800

    feat: set up appollo graph ql client

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import App from './app/app';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
