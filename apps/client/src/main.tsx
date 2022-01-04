6d1d3a591dba1af2fa4ddb33dc0ed501ef4b77a2
ThianHooi <63966221+ThianHooi@users.noreply.github.com>
2022-01-04 11:15:30 +0800

    feat: added auth header in appollo client

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,11 +1,32 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Cookie from 'js-cookie';
 
 import App from './app/app';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:8000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = Cookie.get('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${JSON.parse(token)}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
